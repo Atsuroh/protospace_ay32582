262bf1567f28cae6546c7bfd24618ea4aa6e5a54
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-16 16:38:48 +0900

    first commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,11 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index]
-
+  before_action :authenticate_user!, except: [:index, :show, :new]
   def index
     @prototypes = Prototype.all
   end
-
   def new
     @prototype = Prototype.new
   end
-
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
@@ -17,41 +14,32 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
-
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
-
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
-
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    if Prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
     end
   end
-
   def destroy
     prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+    if prototype.destroy
+      redirect_to root_path
     end
   end
-
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 end
